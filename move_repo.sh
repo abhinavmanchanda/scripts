451213a2d1e3377dee077f93daef17944958fda8
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2016-11-25 14:15:17 +0530

    Showing usage on error and -h

--- move_repo.sh
+++ move_repo.sh
@@ -1,3 +1,22 @@
+function usage {
+ echo ""
+ echo "Usage: \033[0;34m `basename $0` <Repository to be moved> <Repository it goes into> \033[0m"
+ echo ""
+ echo "Example: If you have a repository called dojos, and another called dojo7, running \033[0;34m ./move_repo.sh dojo7 dojos \033[0m will copy dojo7 into dojos as a first level subdirectory, and maintain the git history."
+ echo ""
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+ usage
+ exit 0
+fi
+
+if [ "$#" -ne 2 ]; then
+ echo "\033[0;31mError: Invalid number of arguments. \033[0m"
+ usage
+ exit 1
+fi
+
 cd $1
 mkdir .$1
 git mv * .$1
